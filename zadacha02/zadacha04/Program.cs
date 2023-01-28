48c73f899989cd390697be2f83f263acdb419371
Alexandr-coder2 <120954936+Alexandr-coder2@users.noreply.github.com>
2023-01-28 15:07:13 +0500

    добавили решение задачи 4

---
+++ zadacha02/zadacha04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Решить задачу на три числа, узнать какое больше
+
+Console.Clear();
+
+Console.Write("Введите число a ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число c ");
+int c = int.Parse(Console.ReadLine()!);
+int max = 0;
+if (max < a)
+{
+    max = a;
+}
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+Console.WriteLine($"{max}");
