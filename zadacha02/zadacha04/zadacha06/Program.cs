7f2ed7131b686e7794cfe1cd7732fb32b52fd6a5
Alexandr-coder2 <120954936+Alexandr-coder2@users.noreply.github.com>
2023-01-28 16:00:02 +0500

    добавили решение задачи 6

---
+++ zadacha02/zadacha04/zadacha06/Program.cs
@@ -0,0 +1,14 @@
+﻿// Решение задачи является ли число чётным
+
+Console.Clear();
+
+Console.Write("Введите число:  ");
+int n = int.Parse(Console.ReadLine()!);
+if (n % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
