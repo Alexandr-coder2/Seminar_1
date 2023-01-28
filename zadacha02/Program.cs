c9a8f0d80cf45832cee26a897b0d09747b79b9f2
Alexandr-coder2 <120954936+Alexandr-coder2@users.noreply.github.com>
2023-01-28 14:14:50 +0500

    добавили решение задачи 2

---
+++ zadacha02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Решить задачу два числа узнать какое больше
+
+Console.Clear();
+
+Console.Write("Введите число a ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b ");
+int b = int.Parse(Console.ReadLine()!);
+int max = a;
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+Console.WriteLine($"{max}");
