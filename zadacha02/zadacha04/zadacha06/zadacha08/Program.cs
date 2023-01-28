7fe934035cbe11e337f3da9a6badb163ec4e9071
Alexandr-coder2 <120954936+Alexandr-coder2@users.noreply.github.com>
2023-01-28 16:48:58 +0500

    добавили решение задачи 8

---
+++ zadacha02/zadacha04/zadacha06/zadacha08/Program.cs
@@ -0,0 +1,17 @@
+﻿// Решить задачу на входе число на выходе чётные числа от 1 до N
+
+Console.Clear();
+
+Console.Write("Введите число N ");
+int N = int.Parse(Console.ReadLine()!);
+int k = 1;
+
+while(k<= N)
+{
+  if(k % 2==0)
+  {
+    Console.Write($"{k} ");
+  }
+  k++;
+} 
+
